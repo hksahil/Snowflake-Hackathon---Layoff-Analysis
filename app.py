cb1a5a036c12dd895d7ac1b8be641c004f435fa7
Sahil-choudhary <officialhksahil@gmail.com>
2023-04-30 15:53:39 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+
+import streamlit as st
+from snowflake.snowpark import Session
+from st_aggrid import AgGrid
+
+st.title('Layoffs Tracker')
+
+# Snowflake Connection
+@st.cache_resource
+def create_session():
+    return Session.builder.configs(st.secrets.snowflake).create()
+
+session = create_session()
+
+# Table Loader
+@st.cache_data
+def snowflake_loader(table_name):
+    table = session.table(table_name)
+    table = table.collect()
+    return table
+
+# Table present in Snowflake
+table_name = "STREAMLIT_DEMO.STREAMLIT.LAYOFFS"
+
+# Displaying data
+df = snowflake_loader(table_name)
+st.dataframe(df)
+AgGrid(df)
